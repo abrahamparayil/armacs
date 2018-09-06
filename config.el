0d656922c7525169b458a0c128e160a2050a8ceb
4br <abrahamraji99@gmail.com>
2018-09-06 22:09:04 +0530

    cutting corners

--- config.el
+++ config.el
@@ -1,14 +1,40 @@
 (setq user-full-name "Abraham Raji")
-(setq user-mail-address "abrahamraji99@gmail.com")
-(setq inhibit-startup-message t)
-;; ido mode
-(setq indo-enable-flex-matching t)
-(setq ido-eveywhere t)
-(ido-mode 1);;ido end
-(defalias 'list-buffers 'ibuffer);;ibuffer
-(tool-bar-mode -1)
-(windmove-default-keybindings)
-(global-linum-mode t)
+  (setq user-mail-address "abrahamraji99@gmail.com")
+  (setq inhibit-startup-message t)
+  ;; ido mode
+  (setq indo-enable-flex-matching t)
+  (setq ido-eveywhere t)
+  (ido-mode 1);;ido end
+  (defalias 'list-buffers 'ibuffer);;ibuffer
+  (tool-bar-mode -1)
+  (windmove-default-keybindings)
+  (global-linum-mode t)
+  ;;matlab
+  (add-to-list 'load-path "/home/guyfawkes/.emacs.d/matlab-emacs-master")
+  (load-library "matlab-load")
+  ;;Menubar
+  (menu-bar-mode -1)
+
+  (defun my-menu-bar-open-after ()
+  (remove-hook 'pre-command-hook 'my-menu-bar-open-after)
+  (when (eq menu-bar-mode 42)
+    (menu-bar-mode -1)))
+
+(defun my-menu-bar-open (&rest args)
+  (interactive)
+  (let ((open menu-bar-mode))
+    (unless open
+      (menu-bar-mode 1))
+    (funcall 'menu-bar-open args)
+    (unless open
+      (setq menu-bar-mode 42)
+      (add-hook 'pre-command-hook 'my-menu-bar-open-after))))
+
+(global-set-key [f10] 'my-menu-bar-open)
+
+(unless (file-expand-wildcards (concat package-user-dir "/org-[0-9]*"))
+(package-install (elt (cdr (assoc 'org package-archive-contents)) 0)))
+(require 'org)
 
 (use-package try
 :ensure t)
@@ -18,43 +44,29 @@
 :config
 (which-key-mode))
 
-(use-package counsel
-:ensure t
-)
+(add-to-list 'load-path "/home/guyfawkes/.emacs.d/org-reveal")
+(require 'ox-reveal)
+;;(use-package ox-reveal
+;;:ensure ox-reveal)
 
-(use-package swiper
-:ensure try
-:config
-(progn
- (ivy-mode 1)
- (setq ivy-use-virtual-buffers t)
- (global-set-key "\C-s" 'swiper)
- (global-set-key (kbd "C-c C-r") 'ivy-resume)
- (global-set-key (kbd "<f6>") 'ivy-resume)
- (global-set-key (kbd "M-x") 'counsel-M-x)
- (global-set-key (kbd "C-x C-f") 'counsel-find-file)
- (global-set-key (kbd "<f1> f") 'counsel-describe-function)
- (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
- (global-set-key (kbd "<f1> l") 'counsel-load-library)
- (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
- (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
- (global-set-key (kbd "C-c g") 'counsel-git)
- (global-set-key (kbd "C-c j") 'counsel-git-grep)
- (global-set-key (kbd "C-c k") 'counsel-ag)
- (global-set-key (kbd "C-x l") 'counsel-locate)
- (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
- (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
- ))
+;(setq org-reveal-root "/home/guyfawkes/.emacs.d/org-reveal/reveal.js")
+(setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
+(setq org-reveal-mathjax t)
 
-(use-package auto-complete
-:ensure t
-:init
-(progn
- (ac-config-default)
- (global-auto-complete-mode t)
- ))
+(use-package htmlize
+:ensure t)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)  ;load and activate packages, including auto-complete
+(ac-config-default)
+(global-auto-complete-mode t)
 
 (use-package org-bullets
   :ensure t
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(use-package liso-theme
+:ensure t
+:config (load-theme 'liso t))
--- init.el
+++ init.el
@@ -1,25 +1,26 @@
 ;;Package Management
 (load "package")
 (package-initialize)
+;;(add-to-list 'package-archives
+;;             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+             '("melpa-stable" . "https://melpa.org/packages/") t)
 
 (setq package-archive-enable-alist '(("melpa" magit f)))
+
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 ;;use-package command
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
-  (package-install 'use-package))
-
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(package-install 'use-package))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (deeper-blue))))
+ '(package-selected-packages (quote (ox-reveal htmlize org use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
