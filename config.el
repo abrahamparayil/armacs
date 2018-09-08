e1a272744ab03e9ea1165655c83a05d008f1f8e0
4br <abrahamraji99@gmail.com>
2018-09-09 00:04:37 +0530

    few improvements

--- config.el
+++ config.el
@@ -1,15 +1,18 @@
 (setq user-full-name "Abraham Raji")
 (setq user-mail-address "abrahamraji99@gmail.com")
+
 (setq inhibit-startup-message t)
-;; ido mode
-(setq indo-enable-flex-matching t)
-(setq ido-eveywhere t)
-(ido-mode 1);;ido end
-(defalias 'list-buffers 'ibuffer);;ibuffer
+(defalias 'list-buffers 'ibuffer)
 (tool-bar-mode -1)
 (windmove-default-keybindings)
 (global-linum-mode t)
-;;Menubar
+(allout-mode) ;;outlining
+(global-font-lock-mode 1) ;;syntax highlighting
+
+(setq indo-enable-flex-matching t)
+(setq ido-eveywhere t)
+(ido-mode 1)
+
 (menu-bar-mode -1)
 (defun my-menu-bar-open-after ()
 (remove-hook 'pre-command-hook 'my-menu-bar-open-after)
@@ -26,12 +29,41 @@
 (add-hook 'pre-command-hook 'my-menu-bar-open-after))))
 (global-set-key [f10] 'my-menu-bar-open)
 
+(add-to-list 'default-frame-alist
+'(font . "DejaVu Sans Mono-10"))
+
+(ac-config-default)
+(global-auto-complete-mode t)
+(add-to-list 'ac-modes 'org-mode)
+
+(use-package flycheck
+:ensure t
+:init
+(global-flycheck-mode t))
+
+(use-package htmlize
+:ensure t)
+
+(use-package liso-theme
+:ensure t
+:config (load-theme 'liso t))
+
+(add-to-list 'load-path "/home/guyfawkes/.emacs.d/matlab-emacs-master")
+(load-library "matlab-load")
+
 (unless (file-expand-wildcards (concat package-user-dir "/org-[0-9]*"))
 (package-install (elt (cdr (assoc 'org package-archive-contents)) 0)))
 (require 'org)
 
-(add-to-list 'load-path "/home/guyfawkes/.emacs.d/matlab-emacs-master")
-(load-library "matlab-load")
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(add-to-list 'load-path "~/.emacs.d/org-reveal")
+(require 'ox-reveal)
+(setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
+(setq org-reveal-mathjax t)
 
 (use-package try
 :ensure t)
@@ -41,25 +73,7 @@
 :config
 (which-key-mode))
 
-(add-to-list 'load-path "~/.emacs.d/org-reveal")
-(require 'ox-reveal)
-(setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
-(setq org-reveal-mathjax t)
-
-(use-package htmlize
-:ensure t)
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(package-initialize)  ;load and activate packages, including auto-complete
-(ac-config-default)
-(global-auto-complete-mode t)
-
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
-(use-package liso-theme
+(use-package elpy
 :ensure t
-:config (load-theme 'liso t))
+:config 
+(elpy-enable))
