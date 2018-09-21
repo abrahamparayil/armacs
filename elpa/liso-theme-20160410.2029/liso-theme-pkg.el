b571cd672532ec7bca5cfb2a184f8545896e8ec6
Abraham Raji <abrahamraji99@gmail.com>
2018-09-21 15:47:00 +0530

    more stuff

--- elpa/liso-theme-20160410.2029/liso-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "liso-theme" "20160410.2029" "Eclectic Dark Theme for GNU Emacs" 'nil :commit "844688245eb860d23043455e165ee24503454c81" :keywords '("theme" "themes") :authors '(("Vlad Piersec" . "vlad.piersec@gmail.com")) :maintainer '("Vlad Piersec" . "vlad.piersec@gmail.com") :url "https://github.com/caisah/liso-theme")
--- elpa/try-20170226.1605/try-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "try" "20170226.1605" "Try out Emacs packages." '((emacs "24")) :commit "271b0a362cadf44d0694628b9e213f54516ef913" :keywords '("packages") :authors '(("Lars Tveito" . "larstvei@ifi.uio.no")) :maintainer '("Lars Tveito" . "larstvei@ifi.uio.no") :url "http://github.com/larstvei/try")
