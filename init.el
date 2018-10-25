d22023a65311d0e8a08afeafc0a6172058c6ee6a
Abraham Raji <32333507+4br@users.noreply.github.com>
2018-10-25 21:10:42 +0530

    Update init.el

--- init.el
+++ init.el
@@ -1,17 +1,1 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-(fset 'yes-or-no-p 'y-or-n-p)
-(global-set-key (kbd "<f5>") 'revert-buffer)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (dmenu dired dired-open dired+ dired-details use-package try org-bullets org liso-theme htmlize flycheck elpy auto-complete auto-compile))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
